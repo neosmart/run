83b919b924f41b7628c86758ec695a4cc4ee87ad
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2022-01-18 10:23:39 -0600

    Add an option to wait for target to exit
    In this mode, run.exe waits for the target to exit before it
    terminates, and bubbles up the exit code instead of the process
    id.

--- run/run.cpp
+++ run/run.cpp
@@ -1,37 +1,37 @@
-//#include <cassert>
+#include <cassert>
 #include <string>
 #include <Windows.h>
 #include "utf8.h"
 #pragma comment(lib, "shell32.lib")
 
-//skip past a single, un-nested, quoted or unquoted argument
-const char *skiparg(const char *args)
+// Skip past a single, un-nested, quoted or unquoted argument
+static const char *
+skipargs(const char *args, int skipCount = 1)
 {
-	if (args[0] == '\'')
-	{
-		++args;
-		while (args[0] && (args++)[0] != '\'');
-	}
-	else if (args[0] == '"')
-	{
-		++args;
-		while (args[0] && (args++)[0] != '"');
-	}
-	else
-	{
-		while (args[0] && (args++)[0] != ' ');
-	}
+	for (int i = 0; i <= skipCount; ++i) {
+		if (args[0] == '\'') {
+			++args;
+			while (args[0] && (args++)[0] != '\'');
+		}
+		else if (args[0] == '"') {
+			++args;
+			while (args[0] && (args++)[0] != '"');
+		}
+		else {
+			while (args[0] && (args++)[0] != ' ');
+		}
 
-	//clear any trailing whitespace
-	while (args[0] && args[0] == ' ' || args[0] == '\t')
-	{
-		++args;
+		// Clear any trailing whitespace
+		while (args[0] && args[0] == ' ' || args[0] == '\t') {
+			++args;
+		}
 	}
 
 	return args;
 }
 
-const std::string unquote(std::string input)
+static const std::string
+unquote(std::string input)
 {
 	if ((input[0] == '\'' || input[0] == '\"') &&
 		input[0] == input[input.size() - 1])
@@ -42,6 +42,15 @@ const std::string unquote(std::string input)
 	return input;
 }
 
+/// <summary>
+/// Invisibly runs the command and parameters specified as arguments.
+/// </summary>
+/// <returns>
+/// If the first argument is /wait, then
+/// the process waits for the launched process to exit and returns the exit code. Otherwise, the process
+/// launches the target and exits immediately, returning the PID of the launched target. <c>-1</c> is
+/// returned in case of an error.
+/// </returns>
 int CALLBACK WinMain(
 	_In_ HINSTANCE hInstance,
 	_In_ HINSTANCE hPrevInstance,
@@ -52,47 +61,59 @@ int CALLBACK WinMain(
 	int argc = 0;
 	auto argv = CommandLineToArgvW(GetCommandLineW(), &argc);
 
-	if (argc <= 1)
-	{
-		//No application specified
-		MessageBox(nullptr, "An application to start was not specified! Correct syntax:\r\n"
-			"run \\path\\to\\app.exe [/param1 ...]", "Invalid command line parameters!", MB_OK);
-		return 0;
+	bool wait = false;
+	int targetIndex = 1;
+	if (lstrcmpW(argv[targetIndex], L"/wait") == 0) {
+		wait = true;
+		++targetIndex;
 	}
 
-	std::string application = unquote(wstring_to_utf8(argv[1]));
-	if (GetFileAttributes(application.c_str()) == INVALID_FILE_ATTRIBUTES)
+	if (argc <= targetIndex)
 	{
-		//Application not found
-		std::string message = "The requested application '";
-		message += application;
-		message += "' was not found!";
+		// No application specified
+		MessageBox(nullptr, "An application to start was not specified! Correct syntax:\r\n"
+			"run  [/wait] \\path\\to\\app.exe [/param1 ...]", "Invalid command line parameters!", MB_OK);
+		return -1;
+	}
+
+	std::string application = unquote(wstring_to_utf8(argv[targetIndex]));
+	if (GetFileAttributes(application.c_str()) == INVALID_FILE_ATTRIBUTES) {
+		// Application not found
+		std::string message = "The requested application '" + application + "' was not found!";
 		MessageBox(nullptr, message.c_str(), "Application not found!", MB_OK);
-		return 0;
+		return -1;
 	}
 
 	SHELLEXECUTEINFO sexi = { 0 };
-	sexi.fMask = SEE_MASK_NOCLOSEPROCESS; //to retrieve hProcess of result
+	sexi.fMask = SEE_MASK_NOCLOSEPROCESS; // to retrieve hProcess of result
 	sexi.cbSize = sizeof(sexi);
 	sexi.lpFile = application.c_str();
 
-	//Fast-forward past our own executable to get at the command-line arguments
-	const char *args = skiparg(GetCommandLine());
-	//skip past the executable we are launching; executable paths (obviously) cannot contain ' or "
-	args = skiparg(args);
+	// Skip past the executable we are launching; executable paths (obviously) cannot contain ' or "
+	const char* args = skipargs(GetCommandLine(), targetIndex);
 	sexi.lpParameters = args;
 
 	//sexi.nShow = SW_NORMAL;
 	sexi.nShow = SW_HIDE;
-	BOOL result = ShellExecuteEx(&sexi);
-
-	if (result == FALSE)
-	{
-		return 0;
+	if (ShellExecuteEx(&sexi) == FALSE) {
+		return -1;
 	}
 
-	auto pid = GetProcessId(sexi.hProcess);
-	CloseHandle(sexi.hProcess); //good form!
+	assert(sexi.hProcess != 0);
 
-	return pid;
+	// If waiting, return the exit code. Otherwise, return the process id.
+	DWORD result = -1;
+	if (wait) {
+		WaitForSingleObject(sexi.hProcess, INFINITE);
+		if (!GetExitCodeProcess(sexi.hProcess, &result)) {
+			result = -1;
+		}
+	}
+	else {
+		result = GetProcessId(sexi.hProcess);
+	}
+
+	CloseHandle(sexi.hProcess); // Good form!
+
+	return result;
 }
--- run/utf8.h
+++ run/utf8.h
@@ -2,14 +2,14 @@
 #include <codecvt>
 #include <string>
 
-// convert UTF-8 string to wstring
+/// Convert UTF-8 string to wstring
 std::wstring utf8_to_wstring(const std::string& str)
 {
 	std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
 	return myconv.from_bytes(str);
 }
 
-// convert wstring to UTF-8 string
+/// Convert wstring to UTF-8 string
 std::string wstring_to_utf8(const std::wstring& str)
 {
 	std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
